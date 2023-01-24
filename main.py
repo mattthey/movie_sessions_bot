a30ce23546606896c205d473f9682f13e37d6910
mnoskov <mnoskov@naumen.ru>
2023-01-25 03:13:31 +0500

    Скачивание и прикрепление картинок. Надо решить вопрос с максимальной длиной подписью к тексту"

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from telegram import InputMediaPhoto
 from telegram._inline.inlinekeyboardbutton import InlineKeyboardButton
 from telegram._inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 from telegram._update import Update
@@ -18,9 +19,11 @@ logging.basicConfig(
 def get_buttons(current_position=0):
     result = []
     if current_position > 0:
-        result.append(InlineKeyboardButton(text="Назад", callback_data=str(current_position - 1)))
+        result.append(InlineKeyboardButton(
+            text="Назад", callback_data=str(current_position - 1)))
     if current_position < len(FILMS):
-        result.append(InlineKeyboardButton(text="Далее", callback_data=str(current_position + 1)))
+        result.append(InlineKeyboardButton(
+            text="Далее", callback_data=str(current_position + 1)))
     return result
 
 
@@ -29,12 +32,14 @@ async def start(update: Update, context):
 
 
 async def sessions(update: Update, context):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=get_pretty_films(),
-        parse_mode=ParseMode.HTML,
-        reply_markup=InlineKeyboardMarkup.from_row(get_buttons())
-    )
+    with open('media/0', 'rb') as photo:
+        await context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=photo,
+            caption=get_pretty_films(),
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup.from_row(get_buttons())
+        )
 
 
 async def list_button(update: Update, context) -> None:
@@ -43,11 +48,12 @@ async def list_button(update: Update, context) -> None:
     await query.answer()
     id = int(query.data)
 
-    await query.edit_message_text(
-        text=get_pretty_films(id),
-        parse_mode=ParseMode.HTML,
-        reply_markup=InlineKeyboardMarkup.from_row(get_buttons(id))
-    )
+    with open('media/' + str(id), 'rb') as f:
+        imp = InputMediaPhoto(f, get_pretty_films(id), ParseMode.HTML)
+        await query.edit_message_media(
+            media=imp,
+            reply_markup=InlineKeyboardMarkup.from_row(get_buttons(id))
+        )
     context.drop_callback_data(query)
 
 
@@ -56,13 +62,13 @@ if __name__ == '__main__':
         .token(os.getenv('bot_token')) \
         .arbitrary_callback_data(True) \
         .build()
-    
+
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
     sessions_handler = CommandHandler('sessions', sessions)
     application.add_handler(sessions_handler)
-    
+
     application.add_handler(CallbackQueryHandler(list_button))
 
     application.run_polling()
--- movie_sessions.py
+++ movie_sessions.py
@@ -1,6 +1,10 @@
+from typing import List
 import requests
 import schedule
 from bs4 import BeautifulSoup
+import os
+import shutil
+from bs4.element import Tag
 
 ATTRS_SEARCH = {
     'valign': 'top',
@@ -31,15 +35,52 @@ class Film:
                f"session : {[str(i) for i in self.sessions]}\n"
 
 
+MEDIA_DIR_NAME = 'media'
+
+
+def download_img(url: str, file_name: str) -> None:
+    """Скачать картинку с файлом в файл с указанным именем
+        :param url адрем для скачивания картинки
+        :param file_name имя файла
+    """
+    if not os.path.exists(MEDIA_DIR_NAME):
+        os.mkdir(MEDIA_DIR_NAME)
+    response = requests.get(url)
+    with open(MEDIA_DIR_NAME + '/' + file_name, 'wb') as f:
+        f.write(response.content)
+
+
+def find_description(element: Tag, wrong_str: List[str]) -> str:
+    """Найти описание фильма
+
+    Args:
+        element (Tag): элемент от которого надо искать описание фильма
+        wrong_str (List[str]): список строк, которые не являются описанием фильма
+
+    Returns:
+        str: описание фильма
+    """
+    cur_element = element
+    cur_text = cur_element.text.strip()
+    while cur_text == '' or cur_text in wrong_str:
+        cur_element = cur_element.next_element
+        cur_text = cur_element.text.strip()
+    return cur_text
+
+
 def get_films():
     r = requests.get('https://www.e1.ru/afisha/kino/')
     html_body = r.content.decode('windows-1251')
     soup = BeautifulSoup(html_body, 'html.parser')
     films = []
-    for td in soup.find_all('td', attrs=ATTRS_SEARCH):
+    for table in soup.find_all('table', attrs={'style': 'padding-bottom: 10px;', 'width': '100%', 'cellspacing': '0', 'border': '0'}):
+        url_img = table.find('img').attrs['src']
+        download_img(url_img, str(len(films)))
+        td = table.find('td', attrs=ATTRS_SEARCH)
         film = Film()
-        film.title, film.genre = [i.contents[0] for i in td.find_all('font')]
-        film.description = td.br.next_element.next_element.text
+        film.title, film.genre = [i.contents[0].strip()
+                                  for i in td.find_all('font')]
+        film.description = find_description(td.br, [film.title, film.genre])
         for tr in td.find('table', attrs={'width': '100%'}).find_all('tr'):
             table = tr.find('table')
             if not table:
@@ -57,7 +98,9 @@ FILMS = get_films()
 
 
 def update_films():
+    """Обновить фильмы"""
     global FILMS
+    shutil.rmtree(MEDIA_DIR_NAME, ignore_errors=True)
     FILMS = get_films()
 
 
@@ -78,4 +121,5 @@ def get_pretty_films(id: int = 0) -> str:
 
 
 if __name__ == '__main__':
+    update_films()
     print(get_pretty_films(0))
